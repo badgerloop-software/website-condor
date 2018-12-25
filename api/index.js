1fa80456f942603001bd69e7d3b35c87e914aedc
LiamMahoney <liammahoney96@gmail.com>
2018-12-25 13:03:16 -0600

    initial api work

---
+++ api/index.js
@@ -0,0 +1,51 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+let https = require('https');
+ 
+// Connection URL
+const url = 'mongodb://localhost:27017';
+ 
+// Database Name
+const dbName = 'playground';
+ 
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+ 
+  const db = client.db(dbName);
+
+  findDocuments(db);
+
+  client.close();
+});
+
+const insertDocuments = function(db, callback) {
+    // Get the documents collection
+    const collection = db.collection('documents');
+    // Insert some documents
+    collection.insertMany([
+      {a : 1}, {a : 2}, {a : 3}
+    ], function(err, result) {
+      assert.equal(err, null);
+      assert.equal(3, result.result.n);
+      assert.equal(3, result.ops.length);
+      console.log("Inserted 3 documents into the collection");
+      callback(result);
+    });
+  }
+
+  function findDocuments(db) {
+    let collection = db.collection('documents');
+
+    collection.find({}).toArray((err, docs) => {
+        for (x of docs) {
+            console.log(`new line: ${x}`);
+            console.log(Object.keys(x));
+            for (let i = 0; i < Object.keys(x).length; i++) {
+                console.log(`${Object.keys(x)[i]} : ${x[Object.keys(x)[i]]}`);
+            }
+        }
+    });
+
+  }
