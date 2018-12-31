386fb3c4c12af25c23ba564817decf11ca0ef03c
LiamMahoney <liammahoney96@gmail.com>
2018-12-30 19:51:14 -0600

    removed duplicate variable

--- public/assets/js/about-team.js
+++ public/assets/js/about-team.js
@@ -1,8 +1,6 @@
 (function getTeamLeads() {
     let xhttp = new XMLHttpRequest();
 
-    let xhttp = new XMLHttpRequest();
-
     xhttp.onreadystatechange = function() {
         if (xhttp.readyState === 4 && xhttp.status === 200) {
             console.log(xhttp.responseText);
