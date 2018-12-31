bb32968746e791a52e710b097c5822a77341f7aa
LiamMahoney <liammahoney96@gmail.com>
2018-12-30 20:49:50 -0600

    initial team lead work

--- public/assets/js/about-team.js
+++ public/assets/js/about-team.js
@@ -2,14 +2,53 @@
     let xhttp = new XMLHttpRequest();
 
     xhttp.onreadystatechange = function() {
-        if (xhttp.readyState === 4 && xhttp.status === 200) {
-            console.log(xhttp.responseText);
-        } else if (xhttp.readyState === 4 && xhttp.status !== 200) {
-            console.log('error');
-        }
+        if (xhttp.readyState === xhttp.DONE) {
+            // if ( xhttp.status === 200) {
+                teamLeadCardDriver(JSON.parse(xhttp.responseText));
+            // } else if (xhttp.readyState === 4 && xhttp.status !== 200) {
+            //     //TODO: decide how to handle errors with
+            // }
+        } 
     }
 
     
     xhttp.open("GET", 'https://dev.badgerloop.com/api/teamleads'); //FIXME: when moved to prod link needs to change
     xhttp.send();
 })();
+
+/* 
+    info: Array of objects holding each team leads information
+*/
+function teamLeadCardDriver(info) {
+    let container = document.createElement("div");
+    container.setAttribute('class', 'team-lead-container');
+    for (let x of info) {
+       container.appendChild(createTeamLeadCard(x));
+    }
+
+    document.getElementById('footer').insertBefore(container);
+}
+
+function createTeamLeadCard(obj) {
+    let card = `
+    <div class='team-lead-card' id='${obj._id}>
+        <div class='team-lead-img'>
+            <img src=/public/images/teamleads/${obj.Picture}
+        </div>
+        <div class='team-lead-name'>
+            ${obj.Name}
+        </div>
+        <div class='team-lead-text'>
+            ${obj.Position}
+        </div>
+        <div class='team-lead-text'>
+            ${obj.Major}
+        </div>
+        <div class='team-lead-text'>
+            ${obj.Year}
+        </div>
+    </div>
+    `;
+
+    return card;
+}
