1eeb87c307f3ccb6f8c3309cec2f1eb768f3e5f8
LiamMahoney <liammahoney96@gmail.com>
2019-01-08 19:53:02 -0600

    mongo connection

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
-let MongoClient = require('mongodb').client;
 let http = require('http');
 let url = require('url');
 let fs = require('fs');
+let MongoClient = require('mongodb').MongoClient;
+
 
 let creds;
 fs.readFile('creds.json', 'utf8', function (err, data) {
@@ -13,7 +14,20 @@ http.createServer((request, response) => {
 	let pathName = url.parse(request.url).pathname;
 
 	if (pathName === "/teamleads" && request.method === "GET") {
-	
+		const client = new MongoClient(creds.dbURL);
+
+		client.connect(function(err) {
+			if (!err) {
+				console.log("Connected successfully to server");
+		
+				const db = client.db(dbName);
+			
+				client.close();
+			} else {
+				console.log(`error: ${err}`);
+			}
+		});
+
 	}	
 
 }).listen(3005);
