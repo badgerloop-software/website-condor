acb8ed9f05c41f8466adcecab701648ef600b2a1
LiamMahoney <liammahoney96@gmail.com>
2019-01-08 20:12:21 -0600

    further testing

--- api/index.js
+++ api/index.js
@@ -14,21 +14,33 @@ http.createServer((request, response) => {
 	let pathName = url.parse(request.url).pathname;
 
 	if (pathName === "/teamleads" && request.method === "GET") {
+		console.log(getTeams());
+	}	
+
+}).listen(3005);
+
+function getTeams() {
+	return new Promise((resolve, reject) => {
 		const client = new MongoClient(creds.dbURL);
 
 		client.connect(function(err) {
 			if (!err) {
 				console.log("Connected successfully to server");
-		
 				const db = client.db(creds.db);
-			
+				
+				db.teamleads.distinct('Teams', (err, data) => {
+					if (!err) {
+						resolve(data);
+					} else {
+						reject(err);
+					}
+				});
+
 				client.close();
 			} else {
 				console.log(`error: ${err}`);
 			}
 		});
-
-	}	
-
-}).listen(3005);
+	});
+}
 
