312af26dc354c7e289c9403f80a338e4d38e2fa9
LiamMahoney <liammahoney96@gmail.com>
2019-01-22 18:40:26 -0600

    Contact page now working

--- public/assets/js/contact.js
+++ public/assets/js/contact.js
@@ -1,5 +1,6 @@
 (function() {
     document.getElementById('contact-category').addEventListener('change', formChange);
+    document.getElementById('contact-submit').addEventListener('click', formSubmit);
 })();
 
 function formChange() {
@@ -36,17 +37,17 @@ function studentForm() {
 
     let teams = document.createElement('div');
     teams.setAttribute('class', 'col-12 additional-form');
-    teams.innerHTML = `<div class="form-label">Team(s) Interested In:</div><input type="text">`;
+    teams.innerHTML = `<div class="form-label">Team(s) Interested In:</div><input class="form-check" class="form-check" type="text" title="Teams Interested In">`;
     document.getElementById("contact-form").insertBefore(teams, document.getElementById("contact-category").parentNode.nextSibling);
 
     let year = document.createElement('div');
     year.setAttribute('class', 'col-6 col-12-xsmall additional-form');
-    year.innerHTML = `<div class="form-label">Year:</div><input type="text">`;
+    year.innerHTML = `<div class="form-label">Year:</div><input class="form-check" type="text" title="Year">`;
     document.getElementById("contact-form").insertBefore(year, document.getElementById("contact-category").parentNode.nextSibling);
 
     let major = document.createElement('div');
     major.setAttribute('class', 'col-6 col-12-xsmall additional-form');
-    major.innerHTML = `<div class="form-label">Major:</div><input type="text">`;
+    major.innerHTML = `<div class="form-label">Major:</div><input class="form-check" type="text" title="Major">`;
     document.getElementById("contact-form").insertBefore(major, document.getElementById("contact-category").parentNode.nextSibling);
 
 }
@@ -55,7 +56,7 @@ function sponsorForm() {
 
     let company = document.createElement('div');
     company.setAttribute('class', 'col-12 additional-form');
-    company.innerHTML = `<div class="form-label">Company Name:</div><input type="text">`;
+    company.innerHTML = `<div class="form-label">Company Name:</div><input class="form-check" type="text" title="Company Name">`;
     document.getElementById("contact-form").insertBefore(company, document.getElementById("contact-category").parentNode.nextSibling);
 
 }
@@ -64,7 +65,98 @@ function mediaForm() {
 
     let media = document.createElement('div');
     media.setAttribute('class', 'col-12 additional-form');
-    media.innerHTML = `<div class="form-label">Organization Name:</div><input type="text">`;
+    media.innerHTML = `<div class="form-label">Organization Name:</div><input class="form-check" type="text" title="Organization Name">`;
     document.getElementById("contact-form").insertBefore(media, document.getElementById("contact-category").parentNode.nextSibling);
 
+}
+
+function formSubmit() {
+    let inputObjects = document.querySelectorAll('.form-check');
+
+    let message = "";
+    let validFlag = true;
+
+    for (x of inputObjects) {
+        x.addEventListener('input', inputChange);
+
+        if (validInput(x.value)) {
+            x.classList.remove('form-error');
+            message += "*" + x.title + ":* " + x.value + "\n";
+        } else {
+            validFlag = false;
+            x.classList.add('form-error');
+        }
+    }
+
+    if (validFlag) sendForm(message);
+
+}
+
+function validInput(x) {
+    if (x === "") return false
+
+    return true;
+}
+
+function sendForm(message) {
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState === 4 && xhttp.status === 200) {
+            displayStatusMessage('success');
+            clearForm();
+        } else if (xhttp.readyState === 4 && xhttp.status !== 200) {
+            //TODO: catch errors from Slack and from our API - set up email
+            displayStatusMessage('error');
+        }
+    }
+
+    xhttp.open("POST", 'https://badgerloop.com/node/contact') //TODO: need full link to server API when implemented.
+    xhttp.send(JSON.stringify(message));
+}
+
+function inputChange(event) {
+
+    if (event.target.value !== "") {
+        event.target.classList.remove('form-error');
+    } else {
+        event.target.classList.add('form-error');
+    }
+
+}
+
+function clearForm() {
+
+    removeForm();
+
+    let inputs = document.querySelectorAll('.form-check');
+
+    for (x of inputs) {
+        x.value = "";
+    }
+    
+}
+
+function displayStatusMessage(message) {
+
+    let button = document.getElementById('contact-submit');
+    let parent = button.parentNode.parentNode.parentNode;
+
+    if (message === "success") {
+        alertMessage = "Message successfully sent!";
+        css = "success-message";
+    } else {
+        alertMessage = "There was an issue sending your message. The Badgerloop website team has been notified.";
+        css = "failure-message";
+    }
+
+    let newNode = document.createElement("div");
+    newNode.setAttribute('class', 'alert ' + css);
+    newNode.innerHTML = "<div class='status-content'>" + alertMessage + "</div><div class='status-close' onclick='closeStatusMessage(this)'><img src='/images/x.svg'></img></div>";
+    parent.parentNode.insertBefore(newNode, parent);
+
+}
+
+function closeStatusMessage(obj) {
+    obj.parentNode.parentNode.removeChild(obj.parentNode);
 }
