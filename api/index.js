387050ee9d59c8b4ef0b7fb50d4b045656fc8dc7
LiamMahoney <liammahoney96@gmail.com>
2019-02-03 13:43:07 -0600

    moving slack API functionality

--- api/index.js
+++ api/index.js
@@ -23,7 +23,59 @@ http.createServer((request, response) => {
 		}).catch( (err) => {
 			//TODO: do something with errors (send to slack channel?). 
 		});
-	}	
+	}
+	
+	if (req.url === '/contact' && req.method === 'POST') {
+        var postData = '';
+
+        req.on('data', function (data) {
+            postData += data;
+        });
+
+        req.on('end', function() {
+            console.log(`Request from client has ended : ${JSON.parse(postData)}`);
+
+            let message = {
+                "text": JSON.parse(postData)
+            };
+
+            let options = {
+                hostname: 'hooks.slack.com',
+                path: '/services/T09PPL10S/BD948FF24/9HsUZ9zw7TtifqC4TPLT8eRB',
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            };
+
+            let request = https.request(options, (response) => {
+                console.log(`Status: ${response.statusCode}`);
+                let slackResponse = "";
+
+                response.on('data', (chunk) => {
+                    console.log("chunk of slack response received");
+                    slackResponse += chunk;
+                });
+
+                response.on('end', () => {
+                    console.log(slackResponse);
+                    res.writeHead(200, {'Content-Type': 'application/json'});
+                    res.end(slackResponse);
+                });
+
+            });
+
+            request.on('error', (e) => {
+				console.log(`error: ${e.message}`);
+				res.writeHead(e.statusCode);
+                res.end(e.message);
+            });
+
+            // write data to request body
+            request.write(JSON.stringify(message));
+            request.end();
+        });
+    }
 
 }).listen(3005);
 
