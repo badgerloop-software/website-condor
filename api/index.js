c14437fe8cd943e0c309abd1956451294a65917f
LiamMahoney <liammahoney96@gmail.com>
2019-02-04 22:40:20 -0600

    initial auto email response

--- api/index.js
+++ api/index.js
@@ -71,7 +71,19 @@ http.createServer((request, response) => {
             req.write(JSON.stringify(message));
             req.end();
         });
-    }
+	}
+	
+	if (pathName === '/emailResponse' && request.method === 'POST') {
+		let postData = "";
+
+		request.on('data', function (data) {
+            postData += data;
+		});
+		
+		request.on('end', function() {
+			sendNewStudentEmail(JSON.parse(postData));
+		});
+	}
 
 }).listen(3005);
 
@@ -144,4 +156,31 @@ function getTeamLeadsDriver(teams) {
 		});
 	});
 
+}
+
+function sendNewStudentEmail(emailAddr) {
+	
+	var stream = fs.createWriteStream("mailScript.sh"); //creates a write stream to mailScript.sh script file
+	stream.once('open', function (fd) { //file descriptor so you will be able to close stream
+		stream.write("(\n"); //open script
+		stream.write("  echo To: " + emailAddr + "\n"); //sets recipient to the address
+		stream.write("  echo From: noreply@badgerloop.com\n"); // sets the from address to noreply@badgerloop.com
+		stream.write("  echo \"Content-Type: text/html; \"\n"); // says the email will be in HTML format
+		stream.write("  echo Subject: Badgerloop Form Submission Autoreply\n"); // sets subject of email
+		stream.write("  echo\n"); 
+		stream.write("  cat mail.html\n"); // points the the mail content file
+		stream.write(") | sendmail -t\n"); // closes the script, then sends the mail using PostFix
+		stream.end(); //finishes writing the file
+	});
+
+	//code to actually execute the script
+	const exec = require('child_process').exec;
+	var yourscript = exec('sh mailScript.sh', //command to run that executes the script
+		(error, stdout, stderr) => {
+			console.log(`${stdout}`);
+			console.log(`${stderr}`);
+			if (error !== null) {
+				console.log(`exec error: ${error}`);
+			}
+	});
 }
