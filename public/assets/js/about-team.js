6a1c5cffdaf2058d76011421a46b9642c83375f6
Luke Houge <lukehouge@gmail.com>
2019-04-08 20:48:09 -0500

    fixing executives on about-team

--- public/assets/js/about-team.js
+++ public/assets/js/about-team.js
@@ -88,11 +88,11 @@ function createTeamTitle(team) {
 
 function executiveLeadDriver(info) {
     let electricalLead = document.getElementById("electricalLead");
-    electricalLead.innerHTML = info.Administrative[0]["Name"];
+    electricalLead.innerHTML = info.Administrative[1]["Name"];
 
     let mechanicalLead = document.getElementById("mechanicalLead");
-    mechanicalLead.innerHTML = info.Administrative[1]["Name"];
+    mechanicalLead.innerHTML = info.Administrative[2]["Name"];
 
     let operationsLead = document.getElementById("operationsLead");
-    operationsLead.innerHTML = info.Administrative[2]["Name"];
+    operationsLead.innerHTML = info.Administrative[3]["Name"];
 }
