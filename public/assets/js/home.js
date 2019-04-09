4ef26e96f75f0cb6e005d79ea32b6d8793e836d4
Luke Houge <lukehouge@gmail.com>
2019-04-08 20:09:50 -0500

    removing video, making digits always 2 long

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -86,21 +86,26 @@ var countDownDate = new Date("April 17, 2019 19:001:00").getTime();
 // Update the count down every 1 second
 var x = setInterval(function() {
 
-  // Get todays date and time
-  var now = new Date().getTime();
+    // Get todays date and time
+    var now = new Date().getTime();
+        
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
     
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-    
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
+    // Time calculations for days, hours, minutes and seconds
+    var days = prepend(Math.floor(distance / (1000 * 60 * 60 * 24)));
+    var hours = prepend(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    var minutes = prepend(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
+    var seconds = prepend(Math.floor((distance % (1000 * 60)) / 1000));
+
+    // always makes two digits
+    function prepend(value) {
+        return (value < 10 ? '0' : '') + value;
+    }
+
   // Output the result in an element with id="demo"
-  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  document.getElementById("countdown").innerHTML = days + "D " + hours + "H "
+  + minutes + "M " + seconds + "S ";
     
   // If the count down is over, write some text 
   if (distance < 0) {
