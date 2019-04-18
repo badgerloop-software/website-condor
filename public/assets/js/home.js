7d7c3fa069bf441e7371a8dacc9e2bef7707a1ad
LiamMahoney <liammahoney96@gmail.com>
2019-04-17 22:45:21 -0500

    removing reveal information

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -40,11 +40,11 @@ function sponsorsCardDriver(info) {
     container.setAttribute('class', 'flex-container');
     let sponsorContainer = document.createElement("div");
     sponsorContainer.setAttribute('class', 'sponsor-container');
-           
+
     for (let x of info['Diamond']) {
         sponsorContainer.appendChild(createSponsorCard(x));
     }
-    
+
 
     container.appendChild(sponsorContainer);
 
@@ -78,40 +78,3 @@ function executiveLeadDriver(info) {
     let operationsLead = document.getElementById("operationsLead");
     operationsLead.innerHTML = info.Administrative[2]["Name"];
 }
-
-
-// Set the date we're counting down to
-var countDownDate = new Date("April 17, 2019 19:001:00").getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-    // Get todays date and time
-    var now = new Date().getTime();
-        
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
-    
-    // Time calculations for days, hours, minutes and seconds
-    var days = prepend(Math.floor(distance / (1000 * 60 * 60 * 24)));
-    var hours = prepend(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-    var minutes = prepend(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
-    var seconds = prepend(Math.floor((distance % (1000 * 60)) / 1000));
-
-    // always makes two digits
-    function prepend(value) {
-        return (value < 10 ? '0' : '') + value;
-    }
-
-  // Output the result in an element with id="countdown", with the unit labels in spans so they can be styled seperately
-    let countdown = `
-        ${days}<span>D </span> ${hours}<span>H </span> ${minutes}<span>M </span> ${seconds}<span>S </span>
-    `;
-  document.getElementById("countdown").innerHTML = countdown;
-    
-  // If the count down is over, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("countdown").innerHTML = "EXPIRED";
-  }
-}, 1000);
