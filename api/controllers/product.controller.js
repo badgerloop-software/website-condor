a3b81f73e55b8db56393a1249c216c44f6aad19d
LukeHouge <lukehouge@gmail.com>
2019-09-17 13:13:37 -0500

    adding get

--- api/controllers/product.controller.js
+++ api/controllers/product.controller.js
@@ -4,3 +4,19 @@ const Product = require('../models/product.model');
 exports.test = function (req, res) {
     res.send('Greetings from the Test controller!');
 };
+
+exports.product_create = function (req, res) {
+    let product = new Product(
+        {
+            name: req.body.name,
+            price: req.body.price
+        }
+    );
+
+    product.save(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.send('Product Created successfully')
+    })
+};
--- api/routes/product.route.js
+++ api/routes/product.route.js
@@ -8,3 +8,5 @@ const product_controller = require('../controllers/product.controller');
 // a simple test url to check that all of our files are communicating correctly.
 router.get('/test', product_controller.test);
 module.exports = router;
+
+router.post('/create', product_controller.product_create);
