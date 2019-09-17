9bd69b5beff9dd70e563b7a29fd403be8208e892
Luke Houge <lukehouge@gmail.com>
2019-09-17 11:51:06 -0500

    fixing error

--- api/app.js
+++ api/app.js
@@ -1,14 +1,13 @@
-// app.js
+//app.js
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const product = require('./routes/product.route'); // Imports routes for the products
-
-// initialize our express app
 const app = express();
+
+app.use('/products', product);
 
 let port = 1234;
 
 app.listen(port, () => {
-    console.log('Server is up and running on port number ' + port);
+    console.log('Server is up and running on port numner ' + port);
 });
