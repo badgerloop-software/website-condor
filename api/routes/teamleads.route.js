7c1d82e9bba78e85460cfdf7d115804bbb7d5049
LukeHouge <lukehouge@gmail.com>
2019-09-17 13:30:09 -0500

    fixing more bugs

--- api/routes/teamleads.route.js
+++ api/routes/teamleads.route.js
@@ -9,7 +9,7 @@ const teamleads_controller = require('../controllers/teamleads.controller');
 router.get('/test', teamleads_controller.test);
 module.exports = router;
 
-router.post('/create', teamleads_controller.teamleads);
+router.post('/create', teamleads_controller.teamleads_create);
 router.get('/:id', teamleads_controller.teamleads_details);
 router.put('/:id/update', teamleads_controller.teamleads_update);
 router.delete('/:id/delete', teamleads_controller.teamleads_delete);
