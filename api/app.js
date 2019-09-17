8020011f3c69c27fa1df0280c8736db29c64884c
LukeHouge <lukehouge@gmail.com>
2019-09-17 13:11:42 -0500

    adding mongoose and bodyparser config

--- api/app.js
+++ api/app.js
@@ -1,9 +1,19 @@
 //app.js
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const product = require('./routes/product.route'); // Imports routes for the products
 const app = express();
 
+// Set up mongoose connection
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/TEST_DB', {useNewUrlParser: true});
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/products', product);
 
 let port = 1234;
