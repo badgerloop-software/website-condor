532f07806433a98d1c339ffc69aacdbb0e71e159
Luke Houge <lukehouge@gmail.com>
2019-09-17 11:39:38 -0500

    adding api for CRUD

---
+++ api/app.js
@@ -0,0 +1,11 @@
+// app.js
+const express = require('express');
+const bodyParser = require('body-parser');
+// initialize our express app
+const app = express();
+
+let port = 1234;
+
+app.listen(port, () => {
+    console.log('Server is up and running on port numner ' + port);
+});
