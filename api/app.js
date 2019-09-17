fdee51c4f7a6128d92ed640a681cc9bcabc79060
Luke Houge <lukehouge@gmail.com>
2019-09-17 11:47:01 -0500

    adding routes

--- api/app.js
+++ api/app.js
@@ -1,11 +1,14 @@
 // app.js
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const product = require('./routes/product.route'); // Imports routes for the products
+
 // initialize our express app
 const app = express();
 
 let port = 1234;
 
 app.listen(port, () => {
-    console.log('Server is up and running on port numner ' + port);
+    console.log('Server is up and running on port number ' + port);
 });
