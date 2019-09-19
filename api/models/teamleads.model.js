9fedc27a778ec7f08fc0cc29dd858fb621c922f0
Luke Houge <lukehouge@gmail.com>
2019-09-18 22:42:00 -0500

    fixing model

--- api/models/teamleads.model.js
+++ api/models/teamleads.model.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let TeamleadsSchema = new Schema({
-    Team: { type: String, required: true, max: 100 },
-    Position: { type: String, required: true, max: 100 },
-    Name: { type: String, required: true, max: 100 },
-    Major: { type: String, required: true, max: 100 },
-    Year: { type: String, required: true, max: 100 },
-    Picture: { type: String, required: true, max: 100 },
+    Team: { type: String, required: false, max: 100 },
+    Position: { type: String, required: false, max: 100 },
+    Name: { type: String, required: false, max: 100 },
+    Major: { type: String, required: false, max: 100 },
+    Year: { type: String, required: false, max: 100 },
+    Picture: { type: String, required: false, max: 100 }
 });
 
 
