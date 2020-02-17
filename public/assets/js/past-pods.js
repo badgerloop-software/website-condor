4a37e617a3db2c1a0b49a12d769d9a134c50d037
Luke Houge <lukehouge@gmail.com>
2020-02-17 06:25:19 +0000

    working on styling for new pod pages

--- public/assets/js/past-pods.js
+++ public/assets/js/past-pods.js
@@ -1,19 +1,22 @@
 import { CountUp } from './countUp.min.js';
 
 window.onload = function() {
-	let statsArray = []
-	let statsValues = [15, 180, 1560.87, 75954.90]
-	let x = 1
-	
+	let statsArray = [];
+	let statsValues = [15, 180, 1560.87, 75954.9];
+	let x = 1;
+
 	for (let i of statsValues) {
 		console.log(i);
-		statsArray.push(new CountUp('stat'+x,i))
-		x++
+		statsArray.push(new CountUp('stat' + x, i));
+		x++;
 	}
-	
-	statsArray.forEach(CountUp => CountUp.start())
 
-}
+	x = statsValues.length + 1;
+	for (let i of statsValues) {
+		console.log(i);
+		statsArray.push(new CountUp('stat' + x, i));
+		x++;
+	}
 
-
-
+	statsArray.forEach(CountUp => CountUp.start());
+};
