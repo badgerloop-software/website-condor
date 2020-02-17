f7cbab123ed2ca1364456fd96adb1d0c97b2f0d3
Luke Houge <lukehouge@gmail.com>
2020-02-17 00:57:15 +0000

    starting work on past pods pages— added stats with countUp.js

---
+++ public/assets/js/past-pods.js
@@ -0,0 +1,19 @@
+import { CountUp } from './CountUp.min.js';
+
+window.onload = function() {
+	let statsArray = []
+	let statsValues = [15, 180, 1560.87, 75954.90]
+	let x = 1
+	
+	for (let i of statsValues) {
+		console.log(i);
+		statsArray.push(new CountUp('stat'+x,i))
+		x++
+	}
+	
+	statsArray.forEach(CountUp => CountUp.start())
+
+}
+
+
+
