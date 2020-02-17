b618a9636e2c093f045dba6cd0c73429e0efbdc9
Luke Houge <lukehouge@gmail.com>
2020-02-17 11:00:55 -0600

    finishing pods-past page with pod 2/3 pics, stats, and facts

--- public/assets/js/past-pods.js
+++ public/assets/js/past-pods.js
@@ -2,7 +2,7 @@ import { CountUp } from './countUp.min.js';
 
 window.onload = function() {
 	let statsArray = [];
-	let statsValues = [15, 180, 1560.87, 75954.9];
+	let statsValues = [15.4, 180, 1560.87, 30968.37, 10.1, 235, 547.81, 24984.74, 6.75, 335, 300, 45000];
 	let x = 1;
 
 	for (let i of statsValues) {
