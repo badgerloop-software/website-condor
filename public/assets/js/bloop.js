0b193f6821034fa909617924c0173bd39cd098d0
meganchoy <60989252+meganchoy@users.noreply.github.com>
2022-02-04 12:11:53 -0600

    Update site to solar (#30)
    * major change to solar
    
    * update all years and majors
    
    * edit image sizes
    
    * updated footer with new image
    
    * unix line endings
    
    * updating sponsors
    
    * updating timeline text
    
    * configuring prettier and formatting all files
    
    * configuring prettier and formatting all files
    
    * test
    
    * adding Husky
    
    * removing unused files
    
    * change order of nav bar so hyperloop archive is at the bottom
    
    * change meta
    
    * quick fix
    
    * typo oops
    
    Co-authored-by: Eric Udlis <udlis.eric@gmail.com>
    Co-authored-by: Luke Houge <lukehouge@gmail.com>

---
+++ public/assets/js/bloop.js
@@ -0,0 +1,16 @@
+function testRequest() {
+  var url = document.getElementById('url-textbox').value;
+
+  let req = new XMLHttpRequest();
+
+  req.onreadystatechange = function () {
+    if (req.readyState === 4 && req.status === 200) {
+      console.log(req.responseText);
+    } else if (req.readyState === 4 && req.status !== 200) {
+      console.log('error');
+    }
+  };
+
+  req.open('GET', 'https://badgerloop.org/api/' + url); //TODO: need full link to server API when implemented.
+  req.send();
+}
---
+++ public/assets/js/past-pods.js
@@ -0,0 +1,19 @@
+import { CountUp } from './countUp.min.js';
+
+window.onload = function () {
+  let statsArray = [];
+  let statsValues = [
+    15.4, 180, 1560.87, 636.7, 10.1, 235, 547.81, 75, 6.75, 335, 300, 238.72,
+    6.9, 199, 306, 185.8,
+  ];
+  let x = 1;
+
+  for (let i of statsValues) {
+    console.log(i);
+    statsArray.push(new CountUp('stat' + x + 'l', i));
+    statsArray.push(new CountUp('stat' + x + 's', i));
+    x++;
+  }
+
+  statsArray.forEach(CountUp => CountUp.start());
+};
